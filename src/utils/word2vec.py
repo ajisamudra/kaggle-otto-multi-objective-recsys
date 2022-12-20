b027cc145ae0b1168ece7e38a761e758a791e3a1
lsamudra3 <lsamudra3@gatech.edu>
2022-12-21 00:23:19 +0700

    experiment with word2vec skipgram wdw50 neg30, result quite the same with neg5

--- src/utils/word2vec.py
+++ src/utils/word2vec.py
@@ -33,13 +33,11 @@ def load_word2vec_cbow_embedding(mode: str = "local"):
 
     if mode == "local":
         emd_path = get_local_word2vec_dir()
-        filepath = (
-            f"{emd_path}/word2vec_local_cbow_vec{VECTOR}_wdw{WINDOW}_neg{NEGATIVE}.kv"
-        )
+        filepath = f"{emd_path}/word2vec_local_cbow_vec64_wdw{WINDOW}_neg{NEGATIVE}.kv"
     else:
         emd_path = get_scoring_word2vec_dir()
         filepath = (
-            f"{emd_path}/word2vec_scoring_cbow_vec{VECTOR}_wdw{WINDOW}_neg{NEGATIVE}.kv"
+            f"{emd_path}/word2vec_scoring_cbow_vec64_wdw{WINDOW}_neg{NEGATIVE}.kv"
         )
 
     # load keyed vectors
@@ -212,6 +210,31 @@ def load_annoy_idx_word2vec_dropn1_vect64_wdw50_embedding(mode: str = "local"):
     else:
         emd_path = get_scoring_word2vec_dir()
         filepath = f"{emd_path}/word2vec_scoring_skipgram_dropn1_vec64_wdw50_neg5.kv"
+
+    # load keyed vectors
+    kvectors = KeyedVectors.load(filepath, mmap="r")
+
+    # create annoy index for search nn
+    aid2idx = {aid: i for i, aid in enumerate(kvectors.index_to_key)}
+    index = AnnoyIndex(64, "angular")
+
+    for aid, idx in aid2idx.items():
+        index.add_item(aid, kvectors.vectors[idx])
+
+    # build annoy index
+    index.build(NTREE)
+
+    return index
+
+
+def load_annoy_idx_word2vec_vect64_wdw50_neg30_embedding(mode: str = "local"):
+
+    if mode == "local":
+        emd_path = get_local_word2vec_dir()
+        filepath = f"{emd_path}/word2vec_local_skipgram_vec64_wdw50_neg30.kv"
+    else:
+        emd_path = get_scoring_word2vec_dir()
+        filepath = f"{emd_path}/word2vec_scoring_skipgram_vec64_wdw50_neg30.kv"
 
     # load keyed vectors
     kvectors = KeyedVectors.load(filepath, mmap="r")
